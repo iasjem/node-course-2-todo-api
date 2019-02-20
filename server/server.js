63e99b6350ddab1f36c7eeb5f41fd2e5440da7de
iasjem <jemimah.beryl.sai@gmail.com>
2019-02-20 17:29:42 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -31,7 +31,17 @@ app.get('/todos', (req, res) => {
     });
 });
 
-// GET / todos
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) return res.status(404).send();
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3999');
