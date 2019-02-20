5716a3bfd759f35974a6377a902ad1d62f80b2e8
iasjem <jemimah.beryl.sai@gmail.com>
2019-02-20 20:39:15 +0800

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,6 +44,17 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) return res.status(404).send();
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) return res.status(404).send();
+        res.status(200).send.send(todo);
+    }).catch((e) => res.status(400).send());
+});
+
 app.listen(PORT, () => console.log('Started on port 3000'));
 
 module.exports = {
